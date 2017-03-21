0b0c2a16f466fde72e6ebd1f9ba1e5835a5cc2f7
kazenetu <rakudafactory@gmail.com>
2017-03-21 23:37:11 +0900

    初回コミット

---
+++ PostgreSQL2POJO/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PostgreSQL2POJO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDBName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(57, 33);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(190, 19);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "localhost";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DBNAME";
+            // 
+            // txtDBName
+            // 
+            this.txtDBName.Location = new System.Drawing.Point(326, 36);
+            this.txtDBName.Name = "txtDBName";
+            this.txtDBName.Size = new System.Drawing.Size(112, 19);
+            this.txtDBName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "USER";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(57, 71);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(104, 19);
+            this.txtUser.TabIndex = 5;
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(251, 71);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.Size = new System.Drawing.Size(104, 19);
+            this.txtPW.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(178, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PASSWORD";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(193, 110);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 8;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 139);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(448, 176);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 340);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtPW);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDBName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDBName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PostgreSQL2POJO/Form1.cs
@@ -0,0 +1,196 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PostgreSQL2POJO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            // テスト用
+            var debugText = new StringBuilder();
+
+            var tc = getTableCols();
+
+            var tableName = string.Empty;
+            var code = new StringBuilder();
+
+            foreach(DataRow row in tc.Rows)
+            {
+                if(tableName != row["table_name"].ToString())
+                {
+                    if(code.Length > 0)
+                    {
+                        var className = snakeCase2CamelCase(tableName, true) + "Entity";
+
+                        //クラス名をつける
+                        code.Insert(0, string.Format("public class {0} ",className) + "{" + Environment.NewLine);
+                        code.AppendLine("}");
+
+                        // TODO ファイル書き出し
+
+                        // TEST
+                        debugText.AppendLine("[" + className + ".java]");
+                        debugText.AppendLine(code.ToString());
+
+                        // Codeのクリア
+                        code.Length = 0;
+                    }
+                    tableName = row["table_name"].ToString();
+                }
+
+                var fieldName = snakeCase2CamelCase(row["column_name"].ToString());
+
+                var orgDataType = row["data_type"].ToString().ToLower();
+                var dataType = "String";
+                if (orgDataType == "integer")
+                {
+                    dataType = "int";
+                }
+                else if (orgDataType == "decimal")
+                {
+                    dataType = "java.math.BigDecimal";
+                }
+                else if (orgDataType == "date")
+                {
+                    dataType = "java.sql.Date";
+                }
+                else if (orgDataType == "time")
+                {
+                    dataType = "java.sql.Time";
+                }
+                else if (orgDataType == "timestamp")
+                {
+                    dataType = "java.sql.Timestamp";
+                }
+                code.AppendLine(string.Format("private {0} {1};", dataType, fieldName));
+
+                var methodBaseName = snakeCase2CamelCase(row["column_name"].ToString(), true);
+                code.AppendLine(string.Format("public void set{0}({1} value) ", methodBaseName, dataType) + "{");
+                code.AppendLine(string.Format("{0} = value;", fieldName));
+                code.AppendLine("}");
+
+                code.AppendLine(string.Format("public {0} get{1}()", dataType, methodBaseName) + "{");
+                code.AppendLine(string.Format("return {0};", fieldName));
+                code.AppendLine("}");
+            }
+            if (code.Length > 0)
+            {
+                var className = snakeCase2CamelCase(tableName, true) + "Entity";
+
+                //クラス名をつける
+                code.Insert(0, string.Format("public class {0} ", className) + "{" + Environment.NewLine);
+                code.AppendLine("}");
+
+                // TODO ファイル書き出し
+
+                // TEST
+                debugText.AppendLine("[" + className + ".java]");
+                debugText.AppendLine(code.ToString());
+
+                // Codeのクリア
+                code.Length = 0;
+            }
+
+
+
+            //テスト用文字列をテキストエリアに格納
+            this.textBox1.Text = debugText.ToString();
+
+        }
+
+        /// <summary>
+        /// スネークケースからキャメルケースに変換
+        /// </summary>
+        /// <param name="srcSnakeCase">変換元</param>
+        /// <param name="isUpper">アッパーキャメルにするか否か</param>
+        /// <returns>キャメルケース</returns>
+        private string snakeCase2CamelCase(string srcSnakeCase,bool isUpper = false)
+        {
+            if (srcSnakeCase.Length <= 0) return "";
+
+            string[] words = srcSnakeCase.Split('_');
+
+            string result = "";
+            for(int i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                if ((i==0 && isUpper) || i > 0)
+                {
+                    word = word[0].ToString().ToUpper() + word.Substring(1).ToLower();
+                }else
+                {
+                    word = word.ToLower();
+                }
+                result += word;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// テーブルとカラムの情報をDataTableで取得する
+        /// </summary>
+        /// <returns></returns>
+        private DataTable getTableCols()
+        {
+            // 接続文字列作成
+            var connectionString = string.Format("Server={0};Port=5432;User Id={1};Password={2};Database={3}",
+                txtURL.Text, txtUser.Text, txtPW.Text, txtDBName.Text);
+
+            var sql = new StringBuilder();
+            sql.AppendLine("select ");
+            sql.AppendLine("  table_name,");
+            sql.AppendLine("  information_schema.columns.data_type,");
+            sql.AppendLine("  column_name");
+            sql.AppendLine("from information_schema.columns");
+            sql.AppendLine("inner join pg_stat_user_tables");
+            sql.AppendLine("  on table_name = pg_stat_user_tables.relname");
+            sql.AppendLine("order by table_name,ordinal_position;");
+
+            var result = new DataTable();
+            result.Columns.Add("table_name", typeof(string));
+            result.Columns.Add("data_type", typeof(string));
+            result.Columns.Add("column_name", typeof(string));
+
+
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    // SELECT
+                    cmd.CommandText = sql.ToString();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = result.NewRow();
+                            foreach (DataColumn column in result.Columns)
+                            {
+                                row[column.ColumnName] = reader[column.ColumnName];
+                            }
+                            result.Rows.Add(row);
+
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
