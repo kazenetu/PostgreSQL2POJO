cc59c12868938c1ca4a79052c3991f42b1899124
kazenetu <rakudafactory@gmail.com>
2017-03-22 20:37:54 +0900

    テーブルコメントとカラムコメントを出力する処理を追加

--- PostgreSQL2POJO/Form1.cs
+++ PostgreSQL2POJO/Form1.cs
@@ -25,37 +25,56 @@ namespace PostgreSQL2POJO
             var tc = getTableCols();
 
             var tableName = string.Empty;
+            var tableComment = string.Empty;
             var privates = new StringBuilder();
-            var code = new StringBuilder();
+            var methods = new StringBuilder();
 
             foreach(DataRow row in tc.Rows)
             {
                 if(tableName != row["table_name"].ToString())
                 {
-                    if(code.Length > 0)
+                    if(methods.Length > 0)
                     {
                         var className = snakeCase2CamelCase(tableName, true) + "Entity";
 
-                        //クラス名とプライベートフィールドをつける
-                        string header = string.Format("public class {0} ", className) + "{" + Environment.NewLine;
-                        header += privates.ToString();
-                        code.Insert(0, header);
-                        code.AppendLine("}");
+
+                        var outputData = new StringBuilder();
+
+                        //クラス名とをつける
+                        outputData.AppendLine("/**");
+                        outputData.AppendLine(" * " + tableComment);
+                        outputData.AppendLine(" */");
+                        outputData.AppendLine(string.Format("public class {0} ", className) + "{");
+
+                        //プライベートフィールドをつける
+                        outputData.AppendLine(privates.ToString());
+
+                        //セッター/ゲッター メソッドをつける
+                        outputData.Append(methods.ToString());
+                        outputData.AppendLine("}");
 
                         // TODO ファイル書き出し
 
                         // TEST
                         debugText.AppendLine("[" + className + ".java]");
-                        debugText.AppendLine(code.ToString());
+                        debugText.AppendLine(outputData.ToString());
 
                         // Codeのクリア
-                        code.Length = 0;
+                        methods.Length = 0;
                         privates.Length = 0;
                     }
                     tableName = row["table_name"].ToString();
+                    tableComment = row["table_comment"].ToString();
                 }
 
+                // プライベートフィールド
                 var fieldName = snakeCase2CamelCase(row["column_name"].ToString());
+                var columnComment = row["column_comment"].ToString();
+                var columnCommentTabIndex = columnComment.IndexOf("\t");
+                if (columnCommentTabIndex > 0)
+                {
+                    columnComment = columnComment.Substring(0, columnCommentTabIndex - 1);
+                }
 
                 var orgDataType = row["data_type"].ToString().ToLower();
                 var dataType = "String";
@@ -79,36 +98,57 @@ namespace PostgreSQL2POJO
                 {
                     dataType = "java.sql.Timestamp";
                 }
+                privates.AppendLine();
+                privates.AppendLine("    /**");
+                privates.AppendLine("     * " + columnComment);
+                privates.AppendLine("     */");
                 privates.AppendLine(string.Format("    private {0} {1};", dataType, fieldName));
 
                 var methodBaseName = snakeCase2CamelCase(row["column_name"].ToString(), true);
-                code.AppendLine(string.Format("    public void set{0}({1} value) ", methodBaseName, dataType) + "{");
-                code.AppendLine(string.Format("        {0} = value;", fieldName));
-                code.AppendLine("    }");
 
-                code.AppendLine(string.Format("    public {0} get{1}()", dataType, methodBaseName) + "{");
-                code.AppendLine(string.Format("        return {0};", fieldName));
-                code.AppendLine("    }");
+                methods.AppendLine();
+                methods.AppendLine("    /**");
+                methods.AppendLine("     * " + columnComment + "を設定");
+                methods.AppendLine("     * @param value 設定値");
+                methods.AppendLine("     */");
+                methods.AppendLine(string.Format("    public void set{0}({1} value) ", methodBaseName, dataType) + "{");
+                methods.AppendLine(string.Format("        {0} = value;", fieldName));
+                methods.AppendLine("    }");
+
+                methods.AppendLine();
+                methods.AppendLine("    /**");
+                methods.AppendLine("     * " + columnComment + "を取得");
+                methods.AppendLine("     * @return ” + fieldName + ”を返す");
+                methods.AppendLine("     */");
+                methods.AppendLine(string.Format("    public {0} get{1}()", dataType, methodBaseName) + "{");
+                methods.AppendLine(string.Format("        return {0};", fieldName));
+                methods.AppendLine("    }");
             }
-            if (code.Length > 0)
+            if (methods.Length > 0)
             {
                 var className = snakeCase2CamelCase(tableName, true) + "Entity";
 
-                //クラス名とプライベートフィールドをつける
-                string header = string.Format("public class {0} ", className) + "{" + Environment.NewLine;
-                header += privates.ToString();
-                code.Insert(0, header);
-                code.AppendLine("}");
+
+                var outputData = new StringBuilder();
+
+                //クラス名とをつける
+                outputData.AppendLine("/**");
+                outputData.AppendLine(" * " + tableComment);
+                outputData.AppendLine(" */");
+                outputData.AppendLine(string.Format("public class {0} ", className) + "{");
+
+                //プライベートフィールドをつける
+                outputData.AppendLine(privates.ToString());
+
+                //セッター/ゲッター メソッドをつける
+                outputData.Append(methods.ToString());
+                outputData.AppendLine("}");
 
                 // TODO ファイル書き出し
 
                 // TEST
                 debugText.AppendLine("[" + className + ".java]");
-                debugText.AppendLine(code.ToString());
-
-                // Codeのクリア
-                code.Length = 0;
-                privates.Length = 0;
+                debugText.AppendLine(outputData.ToString());
             }
 
 
@@ -160,17 +200,25 @@ namespace PostgreSQL2POJO
             var sql = new StringBuilder();
             sql.AppendLine("select ");
             sql.AppendLine("  table_name,");
+            sql.AppendLine("  pd.description as table_comment,");
             sql.AppendLine("  information_schema.columns.data_type,");
-            sql.AppendLine("  column_name");
+            sql.AppendLine("  column_name,");
+            sql.AppendLine("  pdc.description as column_comment");
             sql.AppendLine("from information_schema.columns");
             sql.AppendLine("inner join pg_stat_user_tables");
             sql.AppendLine("  on table_name = pg_stat_user_tables.relname");
+            sql.AppendLine("inner join pg_description pd");
+            sql.AppendLine("  on pg_stat_user_tables.relid = pd.objoid and pd.objsubid = 0");
+            sql.AppendLine("inner join pg_description pdc");
+            sql.AppendLine("  on pg_stat_user_tables.relid = pdc.objoid and pdc.objsubid = ordinal_position");
             sql.AppendLine("order by table_name,ordinal_position;");
 
             var result = new DataTable();
             result.Columns.Add("table_name", typeof(string));
+            result.Columns.Add("table_comment", typeof(string));
             result.Columns.Add("data_type", typeof(string));
             result.Columns.Add("column_name", typeof(string));
+            result.Columns.Add("column_comment", typeof(string));
 
 
             using (var conn = new NpgsqlConnection(connectionString))
